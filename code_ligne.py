7b767aaae0682ddc102a9d1d202fd071a649a934
poclaire kenmogne <poclaire1986@gmail.com>
2019-03-16 08:17:26 -0400

    ajout de la bibliothe

---
+++ code_ligne.py
@@ -0,0 +1,22 @@
+import pandas as pd
+import os
+import numpy as numpy
+import librosa 
+
+
+
+def row_modified(row):
+    row['c']=aa
+    return row
+
+if __name__ == "__main__":
+    df=  {'col1': [1,2,3], 'col2': [3,4,5], 'col3': [4,5,6]} 
+    df = pd.DataFrame(data = df)
+    df['a']=[1,1,1]
+    df.apply(lambda x: row_modified(x), axis =1)
+    print(df)
+    
+
+
+
+    
